ec8b8d82f7a293f1926899eb4e79fd913acda586
unknown <maheshvjogi.xworkz@gmail.com git config --global user.email maheshvjogi.xworkz@gmail.com>
2021-12-27 13:25:47 +0530

    Test Team

---
+++ TestTeam.java
@@ -0,0 +1,17 @@
+class TestTeam{
+	public static void main(String[] virat){
+	System.out.println("open main()");
+	for(int i=0;i<10;i++){
+		System.out.print("\t"+virat[i]);
+		}
+	System.out.println();
+	for(int i=10;i<virat.length;i++){
+		System.out.print("\t"+virat[i]);
+		}
+	System.out.println();
+	System.out.print("main() closed");
+	}
+}
+
+		
+	
